6e4a78b95c10cb014a17bdb71dfc8082d9756bfa
JaeYaNe <135186091+JaeYaNe@users.noreply.github.com>
2024-10-03 06:04:30 +0900

    commit

--- BJ2751_수정렬하기2.java
+++ BJ2751_수정렬하기2.java
@@ -6,11 +6,25 @@ import java.util.Arrays;
 public class BJ2751_수정렬하기2 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
-		for(int i=0;i<n;i++) arr[i] = Integer.parseInt(br.readLine());
-		
-		Arrays.parallelSort(arr);
-		for(int i=0;i<n;i++) System.out.println(arr[i]);
+
+		int[] counting = new int[2000001]; 
+
+
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			counting[num + 1000000]++;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < counting.length; i++) {
+			while(counting[i] > 0) {
+				sb.append(i - 1000000).append('\n');
+				counting[i]--;
+			}
+		}
+
+		System.out.print(sb.toString());
 	}
 }
