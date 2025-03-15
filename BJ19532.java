5c5d55e15763e0490163e0fd1bb74de98f85f36f
JaeYaNe <135186091+JaeYaNe@users.noreply.github.com>
2025-03-15 16:59:39 +0900

    코드업로드

--- BJ19532.java
+++ BJ19532.java
@@ -1,39 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class BJ19532 {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		br.close();
-	
-		int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        int f = Integer.parseInt(st.nextToken());
-		
-        int x = 0, y = 0;
-        boolean found = false;
-        
-        for (int i = -999; i <= 999; i++) {
-            for (int j = -999; j <= 999; j++) {
-                if ((a * i + b * j == c) && (d * i + e * j == f)) {
-                    x = i;
-                    y = j;
-                    found = true;
-                    break;
+    static int countRepaint(char[][] board, int startRow, int startCol, char[][] pattern1, char[][] pattern2) {
+        int count1 = 0, count2 = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[startRow + i][startCol + j] != pattern1[i][j]) {
+                    count1++;
+                }
+                if (board[startRow + i][startCol + j] != pattern2[i][j]) {
+                    count2++;
                 }
             }
-            if (found) {
-                break;
+        }
+        return Math.min(count1, count2);
+    }
+
+    static int minRepaintChessboard(char[][] board, int n, int m) {
+        char[][] pattern1 = {
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+        };
+
+        char[][] pattern2 = {
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+            {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+            {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+
+        int minRepaint = Integer.MAX_VALUE;
+        for (int i = 0; i <= n - 8; i++) {
+            for (int j = 0; j <= m - 8; j++) {
+                int repaintCount = countRepaint(board, i, j, pattern1, pattern2);
+                minRepaint = Math.min(minRepaint, repaintCount);
             }
         }
-        System.out.println(x + " " + y);
-	}
+
+        return minRepaint;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        scanner.nextLine();
+
+        char[][] board = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            board[i] = line.toCharArray();
+        }
+
+        int result = minRepaintChessboard(board, n, m);
+        System.out.println(result);
+    }
 }
